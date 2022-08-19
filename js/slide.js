bcea29908dc4115ca9bf981c7dfbe309e82b950f
siham heddi <siham.heddi.dev@gmail.com>
2022-08-19 12:49:03 +0100

    book hotels with slick js

---
+++ js/slide.js
@@ -0,0 +1,40 @@
+$('.hotels').slick({
+    infinite: true, 
+    centerMode: true,
+    centerPadding: '60px',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          centerPadding: '40px',
+          dots: true,
+          arrows:true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows:true,
+          centerPadding: '40px',
+        }
+      },
+    
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+                  
+  
